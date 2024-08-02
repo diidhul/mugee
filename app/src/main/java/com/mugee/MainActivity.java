fa60fb7f1443206e3fe5ef2853670cf0eb43d532
diidhul <fadhil.elsafer@gmail.com>
2024-08-02 16:30:02 +0700

    fix the chat button on payment page

--- app/src/main/java/com/mugee/MainActivity.java
+++ app/src/main/java/com/mugee/MainActivity.java
@@ -1,56 +1,71 @@
 package com.mugee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Bitmap;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView mywebView;
+    private WebView myWebView;
+    private boolean isWhatsAppRedirected = false; // Flag untuk mengelola redirect ke WhatsApp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mywebView=(WebView) findViewById(R.id.webview);
-        mywebView.setWebViewClient(new WebViewClient());
-        mywebView.getSettings().setJavaScriptEnabled(true);
-        mywebView.loadUrl("https://www.mugee.id/");
-        mywebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
-        mywebView.getSettings().setDomStorageEnabled(true);
-        WebSettings webSettings = mywebView.getSettings();
+        myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
 
+        myWebView.setWebViewClient(new MyWebClient());
+        myWebView.loadUrl("https://www.mugee.id/");
     }
 
-    public class mywebClient extends WebViewClient {
+    private class MyWebClient extends WebViewClient {
+
         @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view,url,favicon);
-        }
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            String url = request.getUrl().toString();
+            if (url.startsWith("https://wa.link/") || url.startsWith("https://api.whatsapp.com/send?phone=")) {
+                if (!isWhatsAppRedirected) {
+                    // Buka chat WhatsApp
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        intent.setPackage("com.whatsapp");
+                        startActivity(intent);
+                        isWhatsAppRedirected = true; // Tandai kalo WA udah di-redirect
+                    } catch (android.content.ActivityNotFoundException e) {
+                        // WhatsApp ga terinstall
+                        Toast.makeText(MainActivity.this, "WhatsApp tidak terinstal di perangkat ini.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return true; // URL udah di handle ga perlu dimuat oleh WebView
+            }
+            isWhatsAppRedirected = false; // Reset flag untuk URL lain
+            view.loadUrl(url); // URL ga perlu di-handle, muat dengan WebView
             return true;
         }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            isWhatsAppRedirected = false; // Reset flag selepas halaman selesai dimuat
+        }
     }
+
     @Override
     public void onBackPressed() {
-        if (mywebView.canGoBack()) {
-            mywebView.goBack();
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
         } else {
             super.onBackPressed();
         }
-
     }
 }
